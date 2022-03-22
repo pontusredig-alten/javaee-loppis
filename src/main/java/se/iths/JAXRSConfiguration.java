d98110c8623008d245372e26c215115be4252c06
pontus-redig-alten <pontus.redig@alten.se>
2022-03-22 14:57:57 +0100

    Lektion 1 220322

--- src/main/java/se/iths/JAXRSConfiguration.java
+++ src/main/java/se/iths/JAXRSConfiguration.java
@@ -3,7 +3,7 @@ package se.iths;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("")
+@ApplicationPath("api/v1")
 public class JAXRSConfiguration extends Application {
 
 }
---
+++ src/main/java/se/iths/rest/ItemRest.java
@@ -0,0 +1,55 @@
+package se.iths.rest;
+
+import se.iths.entity.Item;
+import se.iths.service.ItemService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("items")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ItemRest {
+
+    @Inject
+    ItemService itemService;
+
+    @Path("new")
+    @POST
+    public Response createItem(Item item) {
+        itemService.createItem(item);
+        return Response.ok(item).build();
+    }
+
+    @Path("update")
+    @PUT
+    public Response updateItem(Item item) {
+        itemService.updateItem(item);
+        return Response.ok(item).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response findItemById(@PathParam("id") Long id) {
+        Item foundItem = itemService.findItemById(id);
+        return Response.ok(foundItem).build();
+    }
+
+    @Path("getall")
+    @GET
+    public Response getAllItems() {
+        List<Item> foundItems = itemService.getAllItems();
+        return Response.ok(foundItems).build();
+    }
+
+    @Path("{id}")
+    @DELETE
+    public Response deleteItem(@PathParam("id") Long id) {
+        itemService.deleteItem(id);
+        return Response.ok().build();
+    }
+
+}
